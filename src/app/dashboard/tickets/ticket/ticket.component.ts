938794c79f92fdc82572ffb99ceab5dea5d90643
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2025-01-04 13:18:14 -0300

    [NG] REVISITING_INPUTS_AND_SIGNALS - feat: (mostly) implementing the ticket component with internal logic using input signals and regular signals for component properties

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { Ticket } from '../tickets.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  ticket = input.required<Ticket>()
+  isTicketOpen = signal<boolean>(false)
 
+  toggleIsTicketOpen() {
+    this.isTicketOpen.set(!this.isTicketOpen())
+  }
 }
